19d29437d9529da32deb75fd1ad22a85d21ed2ae
András Geiszl <geiszla@gmail.com>
2020-02-04 17:03:42 +0000

    Upload project files

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='vision-compare',
+    version='0.0.1',
+    description='Project for comparing real-time object detection models.',
+    author='Andras Geiszl',
+    packages=[]
+)
