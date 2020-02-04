0846d8d9ceee1d1721a817d2d88b69bfb115cc93
András Geiszl <geiszla@gmail.com>
2020-02-04 18:50:09 +0000

    Update python package and dependencies

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(
     version='0.0.1',
     description='Project for comparing real-time object detection models.',
     author='Andras Geiszl',
-    packages=[]
+    packages=['lib/deep_sort_yolov3']
 )
