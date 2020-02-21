9714fc1437c9ba34e73c973a024e8b39c5592d0e
András Geiszl <geiszla@gmail.com>
2020-02-21 20:10:24 +0000

    Fix benchmark and prettify logging

--- src/benchmark.py
+++ src/benchmark.py
@@ -9,35 +9,9 @@ from PIL import Image
 from __init__ import initialize_environment, PROJECT_PATH
 from utilities import get_image_data, process_predictions, print_boxes, print_debug, ProcessedResult
 
-initialize_environment()
-
-
 RunnerResult = Tuple[object, ProcessedResult]
 
-
-def benchmark():
-    image = Image.open(r'D:\Users\Andris\Desktop\vision-compare\data\COCO\images\000000000110.jpg')
-
-    predictions = run_yolo(image)
-    # predictions = run_squeezedet(image)
-    # predictions = run_ssd(image)
-    # predictions = run_retinanet(image)
-
-    print_debug('{} boxes found'.format(len(predictions[0])))
-
-    print_debug('Loading class names...')
-    with open('res/coco_classes.txt') as classes_file:
-        class_names = classes_file.readlines()
-    class_names = [class_name.strip() for class_name in class_names]
-
-    processed_predictions = process_predictions(predictions, class_names, image)
-    print_boxes(processed_predictions)
-
-    print_debug('Exiting...')
-    sys.exit()
-
-
-benchmark()
+initialize_environment()
 
 
 # Model Runner Functions
@@ -47,13 +21,13 @@ def run_yolo(image: Image) -> RunnerResult:
 
     yolo_directory = path.join(PROJECT_PATH, 'lib/keras_yolo3')
     chdir(yolo_directory)
-    print_debug('Changed to YOLOv3 directory: ' + yolo_directory)
+    print_debug('\nChanged to YOLOv3 directory: ' + yolo_directory)
 
-    print_debug('Loading YOLOv3 model...')
+    print_debug('Loading YOLOv3 model...\n')
     model = YOLO(**{'model_path': path.join(PROJECT_PATH, 'model_data/yolov3.h5')})
     image_data = get_image_data(image, model.model_image_size)  # pylint: disable=no-member
 
-    print_debug('Running predictions on "{}"'.format(image.filename))
+    print_debug('\nRunning predictions on "{}"\n'.format(image.filename))
     predictions = model.sess.run(
         [model.boxes, model.scores, model.classes],
         feed_dict={
@@ -81,3 +55,32 @@ def run_yolo(image: Image) -> RunnerResult:
 
 # def run_retinanet(image: Image) -> RunnerResult:
 #     model = load_model('model_data/retinanet.h5', backbone_name='resnet50')
+
+
+# Benchmark
+
+def benchmark():
+    image = Image.open(r'D:\Users\Andris\Desktop\vision-compare\data\COCO\images\000000000110.jpg')
+
+    predictions = run_yolo(image)
+    # predictions = run_squeezedet(image)
+    # predictions = run_ssd(image)
+    # predictions = run_retinanet(image)
+
+    chdir(PROJECT_PATH)
+    print_debug('\nChanged back to project directory: {}\n'.format(PROJECT_PATH))
+
+    print_debug('{} boxes found'.format(len(predictions[0])))
+    print_debug('Loading class names...')
+    with open('res/coco_classes.txt') as classes_file:
+        class_names = classes_file.readlines()
+    class_names = [class_name.strip() for class_name in class_names]
+
+    processed_predictions = process_predictions(predictions, class_names, image)
+    print_boxes(processed_predictions)
+
+    print_debug('\nExiting...')
+    sys.exit()
+
+
+benchmark()
