6b001bbe4558bbad7523eb69ee1b82490aa58ebc
András Geiszl <geiszla@gmail.com>
2020-02-22 20:30:23 +0000

    Add comments to performance evaluator

--- src/detector.py
+++ src/detector.py
@@ -43,7 +43,7 @@ class Detector():
             }
         )
 
-    def evaluate(self, images_path: str):
+    def evaluate(self, images_path: str) -> None:
         from lib.squeezedet_keras.main.config.create_config import squeezeDet_config
         from lib.squeezedet_keras.main.model.dataGenerator import generator_from_data_path
         from lib.squeezedet_keras.main.model.evaluation import evaluate
@@ -70,11 +70,13 @@ class Detector():
             print_debug(f'{CLASS_NAMES[index]}: precision - {precision} '
                 f'recall - {recall}, f1 - {f1_score}, AP - {average_precision}')
 
-    def evaluate_performance(self, video_path: str, display: bool = False) -> int:
+    def evaluate_performance(self, video_path: str, display: bool = False) -> None:
+        # Open video feed
         video = cv2.VideoCapture(video_path)
         if not video.isOpened():
             raise IOError("Couldn't open webcam or video")
 
+        # Initialize timings and fps counter
         elapsed_time = 0.0
         frames_in_second = 0
         previous_time = time.time()
@@ -82,12 +84,15 @@ class Detector():
         fps_measurements = []
 
         while True:
+            # Read a frame from video
             result, frame = video.read()
             if not result:
                 break
 
+            # Run object detection using the current model (self)
             image = self.detect_image(Image.fromarray(frame))
 
+            # Calculate elapsed and detection time
             current_time = time.time()
             detection_time = current_time - previous_time
             previous_time = current_time
@@ -96,11 +101,13 @@ class Detector():
             frames_in_second = frames_in_second + 1
 
             if elapsed_time > 1:
+                # If one second elapsed, save current fps measurement and reset fps counter
                 elapsed_time = elapsed_time - 1
                 fps_measurements.append(frames_in_second)
                 frames_in_second = 0
 
             if display:
+                # If display is turned on, show the current detection result (bounding box on image)
                 result = numpy.asarray(image)
                 cv2.putText(result, text=f'FPS: {fps_measurements[-1]}', org=(3, 15),
                     fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.50, color=(255, 0, 0),
@@ -108,7 +115,9 @@ class Detector():
                 cv2.namedWindow("result", cv2.WINDOW_NORMAL)
                 cv2.imshow("result", result)
 
-                if cv2.waitKey(1) & 0xFF == ord('q'):
-                    break
+            # Interrupt performance evaluation if q is pressed
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
 
-        return int(statistics.mean(fps_measurements))
+        # Print the mean of all the measured fps values
+        print_debug(f'Mean FPS: {int(statistics.mean(fps_measurements))}')
